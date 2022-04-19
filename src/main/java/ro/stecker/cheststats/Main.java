fa65ef04b620c096b9bc9f5b03770f2fa378a076
Stogalosu <dumisteck@gmail.com>
2022-04-19 10:36:28 +0300

    Add "Inventory Framework" library

--- src/main/java/ro/stecker/cheststats/Main.java
+++ src/main/java/ro/stecker/cheststats/Main.java
@@ -1,12 +1,22 @@
 package ro.stecker.cheststats;
 
+import com.github.stefvanschie.inventoryframework.gui.GuiItem;
+import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
+import com.github.stefvanschie.inventoryframework.gui.type.util.Gui;
+import com.github.stefvanschie.inventoryframework.pane.StaticPane;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getConsoleSender().sendMessage("Hello World");
+        getLogger().info("Hello World");
+        ChestGui gui = new ChestGui(5, "Test");
+        StaticPane pane = new StaticPane(0, 0, 9, 5);
+        pane.addItem(new GuiItem(new ItemStack(Material.ACACIA_BOAT)), 1, 1);
+        gui.addPane(pane);
 
     }
 
